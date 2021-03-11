c2221dd120f5715b16ecf7420f823ab6b1fc762f
Zeynep Karakoyun <80275484+zeynepkarakoyun@users.noreply.github.com>
2021-03-11 19:00:47 +0300

    Add files via upload

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,14 @@
-#Explain your work
+CV1 = {"Name" : "Zeynep" , "Surname" : "Karakoyun" , "Age" : "21" , "Degree" : "University" , "English" : "Intermediate"}
+print(CV1)
 
-#Question 1
-for x in range(a):
-	print(a)
+CV2 = {"Name" : "Gamze" , "Surname" : "Karakoyun" , "Age" : "26" , "Degree" : "Grad" , "English" : "Elemantry"}
+print(CV2)
+
+CV3 = {"Name" : "İsmihan" , "Surname" : "Karakoyun" , "Age" : "28" , "Degree" : "Bachelor" , "English" : "Upper-intermediate" }
+print(CV3)
+
+CV4 = {"Name" : "Murat" , "Surname" : "Demirer" , "Age" : "32" , "Degree" : "Bachelor" , "English" : "Upper-intermediate" }
+print(CV4)
+
+CV5 = {"Name" : "Emre" , "Surname" : "Taşaltın" , "Age" : "22" , "Degree" : "Student" , "English" : "Pre-intermediate" }
+print(CV5)
